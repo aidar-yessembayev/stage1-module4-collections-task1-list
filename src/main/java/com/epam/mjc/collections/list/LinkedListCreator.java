3a6c2fc6318d0e5665d330a0d204c809d5b8c9e4
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-24 11:19:41 +0600

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+
+        if (sourceList.size() > 0) {
+            for (int i = 0; i < sourceList.size(); i++) {
+                int number = sourceList.get(i);
+
+                if (number % 2 == 0) {
+                    result.addLast(number);
+
+                } else {
+                    result.addFirst(number);
+                }
+            }
+        }
+
+        return result;
     }
 }
